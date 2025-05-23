3b4b767e36c74be0c1e8d8bfb9cc9536a1c1fe0e
ĐỖ NGỌC PHI <phido3107@gmail.com>
2025-05-23 14:26:11 +0700

    Update

--- app/database.py
+++ app/database.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
-client = MongoClient("mongodb+srv://pcq3014:bigdata@mealtracker.9ged2qc.mongodb.net/")
+client = MongoClient("mongodb+srv://phido0410:bigdata@mealtracker.9ged2qc.mongodb.net/")
 db = client["meal_tracker"]
 meals_col = db["meals"]
 logs_col = db["logs"]
